545cc7d9508c8eec64d01ea0f977d8a8d3102166
Giovanny Piovesan <giowrp@gmail.com>
2020-01-13 19:29:04 -0300

    added react render and favicon

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Main = () => (
+  <div>
+    Home
+  </div>
+);
+
+export default Main;
+
+ReactDOM.render(<Main />, document.getElementById('root'));
