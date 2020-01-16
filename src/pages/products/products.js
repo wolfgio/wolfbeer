d24b1618ff41256a77c628226d8aeaaf812c055d
Giovanny Piovesan <giowrp@gmail.com>
2020-01-16 04:14:37 -0300

    added filters for product query and listed products

--- src/pages/products/products.js
+++ src/pages/products/products.js
@@ -3,43 +3,57 @@ import { useQuery } from '@apollo/react-hooks';
 import queryString from 'query-string';
 
 import GPSIcon from '../../assets/gps.png';
+import LoadingCard from '../../components/loadingCard/loadingCard';
 
-import { history } from '../../lib/utils';
+import { history, useDebounce } from '../../lib/utils';
 import { SEARCH_CATEGORIES, LIST_PRODUCTS } from './queries';
 import {
   Container,
   AddressContainer,
   ChangeAddressButton,
   LocationIcon,
+  CardGrid,
+  SearchInput,
+  CategorySelect,
 } from './styles';
+import ProductCard from '../../components/productCard/productCard';
+
+const RenderLoadingCards = () => {
+  const cards = [];
+  for (let i = 0; i <= 12; i += 1) {
+    cards.push(<LoadingCard key={i} />);
+  }
+  return cards;
+};
 
 const Products = () => {
-  const [search, setSearch] = useState('');
+  const { debouncedValue, setValue } = useDebounce(600);
+  const [categoryId, setCategoryId] = useState(null);
   const parsedQueryParams = queryString.parse(window.location.search);
 
   const popScreen = () => history.goBack();
 
+  const handleInputChange = (e) => setValue(e.target.value);
+  const handleCategoryChange = (e) => setCategoryId(e.target.value);
+
   const {
     data: categories,
     loading: categoriesLoading,
-    error: categoriesError,
-    refetch: refetchCategories,
   } = useQuery(SEARCH_CATEGORIES);
 
   const {
     data: products,
     loading: productsLoading,
     error: productsError,
-    refetch: refetchProducts,
   } = useQuery(LIST_PRODUCTS, {
     variables: {
       id: parsedQueryParams.poc_id,
-      search: '',
-      categoryId: null,
+      search: debouncedValue,
+      categoryId: categoryId === 'all' ? null : categoryId,
     },
   });
 
-  console.log(products);
+  console.log(categoryId);
 
   return (
     <Container>
@@ -50,6 +64,30 @@ const Products = () => {
           {parsedQueryParams.address}
         </ChangeAddressButton>
       </AddressContainer>
+      <h4 style={{ margin: '16px 0' }}>Filtrar:</h4>
+      <CategorySelect onChange={handleCategoryChange}>
+        <option value="all">Todas</option>
+        {categories && categories.allCategory.map((category) => (
+          <option value={category.id} key={category.id}>
+            {category.title}
+          </option>
+        ))}
+      </CategorySelect>
+      <SearchInput
+        placeholder="Pesquisar produto..."
+        type="search"
+        onChange={handleInputChange}
+      />
+      <CardGrid>
+        {productsLoading && RenderLoadingCards()}
+        {products && products.poc && products.poc.products.length > 0
+          && products.poc.products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        {!productsLoading && (!products || !products.poc || products.poc.products.length <= 0) && (
+          <h2>Não encontramos nenhum produto :/</h2>
+        )}
+      </CardGrid>
     </Container>
   );
 };
--- src/pages/products/styles.js
+++ src/pages/products/styles.js
@@ -6,7 +6,9 @@ export const Container = styled.div`
   display: flex;
   align-items: flex-start;
   flex-flow: column wrap;
-  padding: 16px;
+  padding: 32px 16px 120px;
+  max-width: 1200px;
+  margin: 0 auto;
 `;
 
 export const AddressContainer = styled.div`
@@ -23,7 +25,7 @@ export const ChangeAddressButton = styled.button`
   outline: none;
   font-size 0.850rem;
   text-decoration: underline;
-  
+
   display: flex;
   align-items: center;
 
@@ -35,4 +37,34 @@ export const ChangeAddressButton = styled.button`
 export const LocationIcon = styled.img`
   height: 16px;
   margin-right: 8px;
+`;
+
+export const CardGrid = styled.div`
+  width: 100%;
+  margin-top: 16px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 16px;
+  grid-column-gap: 16px;
+`;
+
+export const SearchInput = styled.input`
+  outline: none;
+  border: none;
+  border-radius: 8px;
+  padding: 8px 16px;
+  box-shadow: 1px 3px 5px 1px rgba(0,0,0,0.2);
+
+  width: 100%;
+  max-width: 320px;
+
+  &::placeholder {
+    opacity: 0.8;
+  }
+`;
+
+export const CategorySelect = styled.select`
+  width: 100%;
+  max-width: 320px;
+  margin-bottom: 16px;
 `;
