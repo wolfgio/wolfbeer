244f59cc84cd88481524472d495493638af59870
Giovanny Piovesan <giowrp@gmail.com>
2020-01-16 00:53:13 -0300

    added new icon contributor

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,6 +11,7 @@ const Footer = () => (
     <p>
       Icons made by
       <a href="https://www.flaticon.com/authors/ddara" title="dDara" rel="noreferrer noopener" target="_blank"> dDara,</a>
+      <a href="https://www.flaticon.com/authors/srip" title="srip"> srip,</a>
       <a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik </a>
       and
       <a href="https://www.flaticon.com/authors/bqlqn" title="bqlqn"> bqlqn </a>
